114feccde71a91a29943ae61de684b334abd4965
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-12-27 18:36:40 +0100

    Update index.js

---
+++ 8kuy/index.js
@@ -0,0 +1,21 @@
+// --------------------------------
+//* Summation
+//!  Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the sample tests.
+
+// For example (Input -> Output):
+// 2 -> 3 (1 + 2)
+// 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+const summation = (n) => (n * (n + 1)) / 2;
+
+// --------------------------------
+
+//! Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+//   Examples(Input -> Output)
+// * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+// * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+// * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+// * [5]                             -> min = 5, max = 5
+
+// --------------------------------
