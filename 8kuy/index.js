96b84a339dd21862b1ec947bee10199b19b405f9
Ivan Tymoshchuk <timohin2231@gmail.com>
2024-01-02 22:54:34 +0100

    Update index.js

--- 8kuy/index.js
+++ 8kuy/index.js
@@ -1,8 +1,6 @@
 // --------------------------------
 
-//!  Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the sample tests.
-
-// For example (Input -> Output):
+//!  Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the 
 // 2 -> 3 (1 + 2)
 // 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
 
